4405e7c3e88e9b025a9c27cdf604b01283da7c5c
Eliton <elitonfilho@gmail.com>
2020-09-30 09:01:59 -0300

    fix ultra-rapida/add fields on sql commit

--- refreshFromPPP/refreshFromPPP.py
+++ refreshFromPPP/refreshFromPPP.py
@@ -13,6 +13,11 @@ class RefreshFromPPP():
     def __init__(self, path, host, port, db_name, user, password):
         self.folder = Path(path)
         self.conn = psycopg2.connect("host='{0}' port='{1}' dbname='{2}' user='{3}' password='{4}'".format(host, port, db_name, user, password))
+        self.map_orbit = {
+            'Ultra-rápida': 2,
+            'Rápida': 3,
+            'Final': 4
+        }
 
     def readPPP(self):
         # Possibility : update the timestamp of measure's beginning from PPP and orbita(has domain)
@@ -24,15 +29,18 @@ class RefreshFromPPP():
                 page = read_pdf.getPage(0)
                 page_content = page.extractText()
                 page_pdf = page_content.replace('\n', '')
+                # print(page_pdf)
                 point['altitude_geometrica'], point['norte'], point['leste'] = re.findall(
                     r'(\-?[0-9]{1,},[0-9]{1,2})([0-9]{7}[.][0-9]{3})([0-9]{6}[.][0-9]{3})', page_pdf)[1]
                 point['altitude_geometrica'] = point['altitude_geometrica'].replace(',', '.')
                 point['altitude_ortometrica'] = re.findall(r'Ortométrica\(m\)(\-?.{1,7})Precis', page_pdf)[0].replace(',', '.')
                 point['cod_ponto'] = re.findall(r'domarco:(.+)Início', page_pdf)[0]
-                point['orbita'] = re.findall(r'dossatélites:\d(\w+)Frequ', page_pdf)[0].capitalize()
+                orbita_str = re.findall(r'dossatélites:\d(\w+-?\w+?)Frequ', page_pdf)[0].capitalize()
+                point['orbita'] = self.map_orbit[orbita_str]
                 point['freq_processada'] = re.findall(r'Frequênciaprocessada:(\w+)Interva', page_pdf)[0]
                 data = re.findall(r'Processadoem:(\d\d/\d\d/\d\d\d\d)', page_pdf)[0]
                 point['data_processamento'] = datetime.strptime(data, '%d/%m/%Y')
+                point['meridiano_central'] = re.findall(r'(-?\d\d)Nadatadolevantamento', page_pdf)[0]
                 lat, lon = re.findall(r'levantamento5(.{2,3}°.{2}´.{7}).(.{2,3}°.{2}´.{7})', page_pdf)[0]
                 point['latitude'], point['longitude'] = self.evaluateCoords(lat, lon)
                 # print(point)
@@ -44,7 +52,7 @@ class RefreshFromPPP():
             UPDATE bpc.ponto_controle_p
             SET norte='{norte}', leste='{leste}', altitude_geometrica='{altitude_geometrica}', altitude_ortometrica='{altitude_ortometrica}',
             freq_processada='{freq_processada}', latitude='{latitude}', longitude='{longitude}', geom=ST_GeomFromText('POINT({longitude} {latitude})', 4674),
-            data_processamento='{data_processamento}'
+            data_processamento='{data_processamento}', meridiano_central='{meridiano_central}', orbita={orbita}
             WHERE cod_ponto='{cod_ponto}'
             '''.format(**point))
             self.conn.commit()
